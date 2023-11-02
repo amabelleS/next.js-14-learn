c858a3aee56a90d1b8ca9c7cca13935a4318c68a
amabelleS <amabelletr@gmail.com>
2023-11-02 03:57:08 +0200

    handle errors: error/not-found.tsx

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,23 +20,29 @@ const DeleteInvoice = InvoiceSchema.omit({ date: true, id: true });
 
 export async function createInvoice(formData: FormData) {
     // another method, If you're working with forms that have many fields:
-    const rawFormData = Object.fromEntries(formData.entries())
+    // const rawFormData = Object.fromEntries(formData.entries())
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
       });
     // Test it out:
-    console.log(rawFormData);
+    // console.log(rawFormData);
     // It's a good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors:
     const amountInCents = amount * 100;
     // create a new date with the format "YYYY-MM-DD":
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
 
     // clear Next.js Client-side Router Cache and trigger a new request to the server. 
     revalidatePath('/dashboard/invoices');
@@ -52,11 +58,15 @@ export async function updateInvoice(id: string, formData: FormData) {
    
     const amountInCents = amount * 100;
    
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+            `;
+    } catch (error) {
+        return { message: 'Database Error: Failed to Update Invoice.' };
+    }
    
     // clear the client cache and make a new server request.
     revalidatePath('/dashboard/invoices');
@@ -65,6 +75,12 @@ export async function updateInvoice(id: string, formData: FormData) {
   }
 
   export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+    throw new Error('Failed to Delete Invoice');
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
   }
