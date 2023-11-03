bd0201269b2151e31ba3af2f7f865dccd41fc14a
amabelleS <amabelletr@gmail.com>
2023-11-03 02:44:30 +0200

    forms: bug with edit-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,76 +6,111 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
  
 const InvoiceSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
-});
+    id: z.string(),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
+  });
  
 // Use Zod to update the expected types
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
 const UpdateInvoice = InvoiceSchema.omit({ date: true });
 const DeleteInvoice = InvoiceSchema.omit({ date: true, id: true });
 
-export async function createInvoice(formData: FormData) {
-    // another method, If you're working with forms that have many fields:
-    // const rawFormData = Object.fromEntries(formData.entries())
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-      });
-    // Test it out:
-    // console.log(rawFormData);
-    // It's a good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors:
-    const amountInCents = amount * 100;
-    // create a new date with the format "YYYY-MM-DD":
-    const date = new Date().toISOString().split('T')[0];
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
 
-    try {
-        await sql`
-            INSERT INTO invoices (customer_id, amount, status, date)
-            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-        `;
-    } catch (error) {
-        return {
-            message: 'Database Error: Failed to Create Invoice.',
-        };
-    }
-
-    // clear Next.js Client-side Router Cache and trigger a new request to the server. 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-}
-
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+  export async function createInvoice(prevState: State, formData: FormData) {
+    // Validate form using Zod
+    const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
    
-    const amountInCents = amount * 100;
-   
-    try {
-        await sql`
-            UPDATE invoices
-            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-            WHERE id = ${id}
-            `;
-    } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
     }
    
-    // clear the client cache and make a new server request.
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+   
+    // Insert data into the database
+    try {
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+      // If a database error occurs, return a more specific error.
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
+   
+    // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
-    // redirect the user to the invoice's page.
     redirect('/dashboard/invoices');
   }
 
+  export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+  ) {
+    console.log("🚀 ~ file: actions.ts:80 ~ formData:", formData)
+    const validatedFields = UpdateInvoice.safeParse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    console.log("🚀 ~ file: actions.ts:86 ~ validatedFields:", validatedFields)
+   
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
+   
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+   
+    try {
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+  
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
